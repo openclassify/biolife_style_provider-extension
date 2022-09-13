72a51c7b97cca21cfdc6c278ebc95d6420d5bfbc
erhanask <erhanasik8@gmail.com>
2022-09-13 10:56:12 +0300

    Added translations.

---
+++ resources/lang/tr/field.php
@@ -0,0 +1,55 @@
+<?php
+
+return [
+    'home' => 'Anasayfa',
+	'search_placeholder' => 'Buradan Ara...',
+	'qty' => 'Qty',
+	'working_hours' => 'Pzt-Cuma: :weekday_start-:weekday_break; Cmt-Pzr: :weekend_start-:weekend_break',
+	'product_message' => 'Tüm ürünler sizlerin sağlığını gözeterek hazırlanır .',
+    'menu' => 'Menü',
+	'bestseller' => [
+		'main_title' => 'En çok satanlar',
+		'sub_title' => 'Sizin için en kaliteliler',
+	],
+	'special_offer' => 'Special Offer',
+	'shop_now' => 'Buy Now',
+	'featured_ads' => [
+		'main_title' => 'Our Products',
+		'sub_title' => 'Choosed For You',
+	],
+	'featured' => 'Öne Çıkanlar',
+	'popular_ads' => 'Popüler İlanlar',
+	'latest_ads' => 'Son İlanlar',
+	'continue_reading' => 'Okumaya Devam Et',
+    'add_to_cart' => 'Sepete Ekle',
+    'items' => 'Ürünler',
+    'cart' => 'Sepet',
+    'cart_no_product' => 'Sepetinizde Ürün Yok',
+    'go' => 'Git',
+	'choose_a_category' => 'Bir Kategori Seçin',
+	'categories' => 'Kategoriler',
+	'share' => 'Paylaş',
+	'latest_article' => [
+		'main_title' => 'Son Yayınlanan Makaleler',
+		'sub_title' => 'En taze ve heyecan veren haberler',
+	],
+
+	// Footer
+	'got_question' => 'Sorum Var',
+	'newsletter' => [
+		'title' => 'Dergimiz için abone olun',
+		'subtitle' => 'Dergimiz ve indirimler için abone olun',
+        'button' => 'Abone Olun',
+	],
+    'contact_info' => 'İletişim Bilgileri',
+    'all_rights_reserved' => 'Tüm Hakları Saklıdır',
+
+    'sku' => 'Sku',
+    'by' => 'By',
+    'product_descriptions' => 'Ürün Açıklamaları',
+    'addtional_information' => 'Ek Bilgiler',
+    'no_description' => 'Açıklama Yok',
+    'quantity' => 'Adet',
+    'submit_your_review' => 'İncelemenizi yayınlayın',
+    'wishlist' => 'İstek Listesi',
+];
